328a2a7829a8f4a9c78932c4ee9ee877bea040cc
Satbir Singh <ribtas007@gmail.com>
2023-01-31 15:28:24 +0530

    added error handler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
-
 import "./css/main.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Root from "./pages/Root";
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
 { path: '/',
   element: <Root />,
+  errorElement: <ErrorPage />,
  children:[
-  { path: '/', element: <Home  />},
+  { index:true, element: <Home  />},
   { path: 'About', element: <About />},
   { path: 'Contact', element: <Contact />}
   ],
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ErrorPage =()=> {
+  return (
+    <>
+    
+    
+    <div className='title'><h2>Something went wrong :(</h2></div>
+    <section>
+    <p> Kindly make sure you are going to the correct website address !</p>
+   
+
+    </section>
+    </>
+  )
+}
+
+export default ErrorPage
